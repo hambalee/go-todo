80e7b3f166bced46ab78b896f1abc0b1a80cca3c
hambalee <hambalee@outlook.com>
2022-11-21 02:23:42 +0700

    http standard library

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/ping", pingpongHandler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func pingpongHandler(w http.ResponseWriter, r *http.Request) {
+	resp := struct {
+		Message string `json:"message"`
+	}{
+		Message: "pong",
+	}
+	json.NewEncoder(w).Encode(&resp)
+}
