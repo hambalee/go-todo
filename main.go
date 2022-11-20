ff23001610c8d3bbea069b22451d401fa5cd29a2
hambalee <hambalee@outlook.com>
2022-11-21 03:30:21 +0700

    gorm

--- main.go
+++ main.go
@@ -2,13 +2,43 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type User struct {
+	gorm.Model
+	Name string
+}
+
 func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
+	db.Create(&User{Name: "Hello"})
+
 	r := gin.Default()
+
+	userHandler := UserHandler{db: db}
+	r.GET("/users", userHandler.User)
+
 	r.SetTrustedProxies([]string{"127.0.0.1"})
 	r.GET("/ping", pingpongHandler)
 	r.Run(":8080")
+}
+
+type UserHandler struct {
+	db *gorm.DB
+}
+
+func (h *UserHandler) User(c *gin.Context) {
+	var u User
+	h.db.First(&u)
+	c.JSON(200, u)
 }
 
 func pingpongHandler(c *gin.Context) {
